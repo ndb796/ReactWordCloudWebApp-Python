a693b50b9fec124e09d4dd79a8d411bce57f55b7
ndb796 <ndb796@naver.com>
2019-01-12 04:35:42 +0900

    Update word_cloud.py

--- word_cloud.py
+++ word_cloud.py
@@ -8,9 +8,14 @@ from collections import Counter
 import matplotlib.pyplot as plt
 # Flask 웹 서버 구축에 필요한 라이브러리를 불러옵니다.
 from flask import Flask, request, jsonify
+# 테스트를 위하여 CORS를 처리합니다.
+from flask_cors import CORS
+# 파일에 접근하기 위한 라이브러리를 불러옵니다.
+import os
 
 # 플라스크 웹 서버 객체를 생성합니다.
 app = Flask(__name__, static_folder='outputs')
+CORS(app)
 
 # 폰트 경로 설정
 font_path = 'NanumGothic.ttf'
@@ -48,7 +53,11 @@ def make_cloud_image(tags, file_name):
     plt.imshow(word_cloud)
     plt.axis("off")
     # 만들어진 이미지 객체를 파일 형태로 저장합니다.
-    fig.savefig("outputs/{0}.png".format(file_name))
+    path = "outputs/{0}.png".format(file_name)
+    # 이미 파일이 존재하는 경우 덮어쓰기합니다.
+    if os.path.isfile(path):
+        os.remove(path)
+    fig.savefig(path)
 
 
 def process_from_text(text, max_count, min_length, words, file_name):
@@ -80,5 +89,18 @@ def output():
     return app.send_static_file(text_id + '.png')
 
 
+@app.route('/validate', methods=['GET',' POST'])
+def validate():
+    text_id = request.args.get('textID')
+    path = "outputs/{0}.png".format(text_id)
+    result = {}
+    # 해당 이미지 파일이 존재하는지 확인합니다.
+    if os.path.isfile(path):
+        result['result'] = True
+    else:
+        result['result'] = False
+    return jsonify(result)
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+    app.run('0.0.0.0', port=5000, threaded=True) # 처리 속도 향상을 위해 쓰레드를 적용합니다.
